c6390e25fe7d76fc03ed5d401c20468f9edbcfc7
Lucas Cantoni <lukcnt@outlook.com>
2022-08-09 00:28:35 -0300

    Added the for loop summing the heights and counting the students.

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ student_heights = input("Input a list of students heights in centimeters and "
 # I didn't use sum and len function purposely to get a better grasp of for loops.
 sum_heights = 0
 total_students = 0
+
+for height in student_heights:
+    sum_heights += int(height)
+    total_students += 1
+
+average_height = round(sum_heights / total_students)
+print(f"The average height of the students are: {average_height} centimeters.")
