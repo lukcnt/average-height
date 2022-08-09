9c0e2359d100c67cd0744fee615d5c504ea93f29
Lucas Cantoni <lukcnt@outlook.com>
2022-08-09 00:11:49 -0300

    Created the variables that will be used on the for loop.

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 student_heights = input("Input a list of students heights in centimeters and "
                         "with a space between each one: ").split()
+
+# I didn't use sum and len function purposely to get a better grasp of for loops.
+sum_heights = 0
+total_students = 0
